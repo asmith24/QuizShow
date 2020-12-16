568ecb582a46615bbc6d7c370bdca1ca637944f1
asmith24 <73917810+asmith24@users.noreply.github.com>
2020-12-15 21:45:47 -0500

    Update main.py
    Fixed Syntax errors

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
+"""" 
+Mad Libs 
+Core 6 
+Alana Smith 
 """
-MadLibs
-Author: 
-Period/Core:
+
+print("Welcome to the Game Show! ")
+
+name = input("What's your name contestant?")
+
+feelings = input("How are you feeling today " + name + "?")
+
+print("\nAwh! Lets get Quizzing!") 
+print("Your Category is: ART")
+
+# ART QUESTIONS 
 
 
-"""
+ans = int(input("\nWhat year was the Mona Lisa Painted?"))
+
+if ans == 1503  :
+    print ("Correct!")
+
+else :
+    print("Nope! Better luck next time.")
+
+
+
+
+anstwo = int(input("\nHow many paintings did Michelangelo make?"))
+
+if anstwo == 182 :
+  print("Correct!")
+
+else :
+  print("Nope! Better luck next time.")
+
+
+ansthree = input('Which artist created "a starry night"?')
+
+if ansthree == 'Vincent Van Gogh':
+  print("Correct!")
+
+else : print("Nope! Better luck next time.")
