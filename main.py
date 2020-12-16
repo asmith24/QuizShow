07468a38ea2c365545abb387ed390140ce70e862
Alana Smith <asmith24@biomedscienceacademy.org>
2020-12-16 02:31:36 +0000

    Error in the main py

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
-"""" 
-Mad Libs 
-Core 6 
-Alana Smith 
 """
-
-print("Welcome to the Game Show! ")
-
-name = input("What's your name contestant?")
-
-feelings = input("How are you feeling today " + name + "?")
-
-print("Awh! Lets get Quizzing!") 
-print("Your Category is: ART")
-
-# ART QUESTIONS 
-
-year = 1503 
-ans = input("\nWhat year was the Mona Lisa Painted?")
-
-if year :
-    print ("Correct!")
-
-else :
-    print("Nope! Better luck next time.")
+MadLibs
+Author: 
+Period/Core:
 
 
-
-many = 182
-anstwo = input("\nHow many paintings did Michelangelo make?")
-
-if anstwo :
-  print("Correct!")
-
-else :
-  print("Nope! Better luck next time.")
+"""
